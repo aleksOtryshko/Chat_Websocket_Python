07f5320b23a4b3be5857b7b8cc41f9253e6c1230
aleksOtryshko <55054459+aleksOtryshko@users.noreply.github.com>
2024-11-10 20:11:53 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,20 @@
+import asyncio
+import websockets
+
+clients = set()
+
+async def handler(websocket):
+    clients.add(websocket)
+    try:
+        async for message in websocket:
+            for client in clients:
+                if client != websocket:
+                    await client.send(message)
+    finally:
+        clients.remove(websocket)
+
+async def main():
+    async with websockets.serve(handler, "localhost", 8765):
+        await asyncio.Future()  # run forever
+
+asyncio.run(main())
